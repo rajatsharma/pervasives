5238003fc6bc8754ec10139e40c7d7feeb16cb77
Rajat Sharma <lunasunkaiser@gmail.com>
2022-10-20 12:21:49 +0530

    add current_dir_name

--- src/file.rs
+++ src/file.rs
@@ -4,3 +4,14 @@ use std::path::PathBuf;
 pub fn relative(path: &str) -> Result<PathBuf, std::io::Error> {
     current_dir().map(|p| p.join(path))
 }
+
+pub fn current_dir_name() -> String {
+    let binding = current_dir().unwrap();
+    binding
+        .file_name()
+        .clone()
+        .unwrap()
+        .to_str()
+        .unwrap()
+        .to_owned()
+}
